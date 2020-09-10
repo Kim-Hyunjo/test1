1f169e67715390ef69699aa4f0ac51ab71e4f776
kimhyunjo <tulip23@naver.com>
2020-09-10 19:44:36 +0900

    feat: add mutiply function

--- calculator.py
+++ calculator.py
@@ -2,8 +2,15 @@ class cal:
     def add(self, a, b):
         print('a + b = ', a+b)
 
+<<<<<<< Updated upstream
     def subtract(self, a, b):
         print('a - b = ', a-b)
+
+    def multiply(self, a, b):
+        print('a * b=', a*b)
     
     def main():
         print('main')
+=======
+  
+
